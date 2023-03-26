d408d2a031851d238307a7c1a36779f79e5acca4
nhm <3059350051@qq.com>
2023-03-26 15:21:11 +0800

    feat 接入elementui-plus

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
+import { h, getCurrentInstance } from 'vue'
+// 获取当前组件上下文
+const { proxy }: any = getCurrentInstance()
 
+proxy.$message({
+  message: h('p', null, [
+    h('span', null, 'Message can be '),
+    h('i', { style: 'color: teal' }, 'VNode'),
+  ]),
+})
 </script>
 
 <template>
   首页
+  <el-button>element按钮</el-button>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
